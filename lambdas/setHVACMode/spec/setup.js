f4f12f87ad4184901259ce6068d93b3ac7d52ebf
Myles Eftos <myles@madpilot.com.au>
2017-09-04 19:33:29 +1000

    Removing the lambda. THanks AWS. And have setup a controller

--- lambdas/setHVACMode/spec/setup.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
-
-delete global.expect;
-global.expect = chai.expect;
-global.sinon = sinon;
-
-chai.use(sinonChai);
